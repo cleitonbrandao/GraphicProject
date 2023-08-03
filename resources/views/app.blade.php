4515b4ef3bec91af004357b1538ef1cfa451c7c9
cleitonbrandao <cbs3dmax@gmail.com>
2023-08-03 20:13:39 -0300

    first comit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @routes
+        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+        @inertiaHead
+    </head>
+    <body class="font-sans antialiased dark:bg-dots-lighter">
+        @inertia
+    </body>
+</html>
+<style>
+    .bg-dots-darker {
+        background-image: linear-gradient(to bottom right, #5e8ab5, #a0b6cc ); /* Defina as cores para criar o degradê */
+    }
+    @media (prefers-color-scheme: dark) {
+        .dark\:bg-dots-lighter {
+            background-image: linear-gradient(to bottom right, #01162b, #344658 ); /* Defina as cores para criar o degradê */
+        }
+    }
+</style>
